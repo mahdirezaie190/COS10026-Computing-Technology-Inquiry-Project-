80ef27653185bfa3a58369b0274cca8fc22ee8be
yashneet-14 <113833141+yashneet-14@users.noreply.github.com>
2023-05-06 18:46:33 +0530

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+// settings.php
+
+// Database connection variables
+define('DB_HOST', 'your_host_here');
+define('DB_USER', 'your_username_here');
+define('DB_PASS', 'your_password_here');
+define('DB_NAME', 'your_database_name_here');
