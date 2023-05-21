206f64b0ca0726de475aa71a4b4f333761c5ec4c
wafflesmilk <126733437+wafflesmilk@users.noreply.github.com>
2023-05-21 19:44:26 +1000

    Add files via upload

---
+++ assign2/assign2/about.php
@@ -0,0 +1,530 @@
+
+<?php 
+$title="About Page";
+include 'header.inc';
+?> 
+
+<body> 
+
+<?php 
+include 'menu.inc';
+?>
+
+    <div class="group">
+        <h1 id="header"> meet the team!</h1>
+        <img src="images/group.jpeg" alt="group">
+        <dl >
+            <dt>GROUP NAME:</dt>
+            <dd>Code Crafters</dd>
+
+            <dt> TUTOR NAME:</dt>
+            <dd>Jingwen Zhou</dd>
+        </dl>
+    </div>
+    <div class="profile">
+        <div class="row1">
+            <dl>
+                <dt>STUDENT NAME: </dt>
+                <dd>Yashneet Kaur</dd>
+        
+                <dt>STUDENT ID: </dt>
+                <dd>104174912</dd>
+        
+                <dt>COURSE: </dt>
+                <dd>Bachelor of Computer Science (Cybersecurity)</dd>
+        
+                <dt>STUDENT EMAIL: </dt>
+                <dd>
+                    <a href="mailto:104174912@student.swin.edu.au">
+                        104174912@student.swin.edu.au
+                    </a>
+                </dd>
+                <dt>SPORTS: </dt>
+                <dd>baseball</dd>
+                <dt>HOBBIES: </dt>
+                <dd>sketching, painting, reading</dd>
+            </dl>
+
+            <img src="images/yashneet.jpeg" alt="yashneet">
+
+            
+        </div>
+        <div class="row2">
+            <table>
+                <tr>
+                        <th>monday</th>
+                        <th>tuesday</th>
+                        <th>wednesday</th>
+                        <th>thursday</th>
+                        <th>friday</th>
+                </tr>
+                <tr>
+                    <td>COS20031 Live online <br> 10:30-11:30
+                    </td>
+
+                    <td>COS10009 Lecture 1 <br> 10:30-12:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10026 Workshop 1 <br> 10:30-12:30
+                    </td>
+
+                    <td>TNE10006 Lecture 1 <br> 10:30-12:30
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>COS10026 Live online <br>  12:30-1:30
+                    </td>
+
+                    <td>COS10026 Class 1 <br> 3:30-4:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10009 Class 1 <br> 12:30-2:30
+                    </td>
+                    
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>COS20031 Class 1 <br> 4:30-5:30
+                    </td>
+
+                    <td></td>
+
+                    <td>TNE10006 Class 1 <br> 2:30-5:30
+                    </td>
+
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                    <td>COS20031 Workshop 1 <br> 6:30-8:30
+                    </td>
+
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+
+
+    <div class="profile">
+        <div class="row1">
+            <dl>
+                <dt>STUDENT NAME:</dt>
+                <dd>Lorraine Becker</dd>
+            
+            
+                <dt>STUDENT ID:</dt>
+                <dd>104584773</dd>
+                
+            
+                <dt>COURSE:</dt>
+                <dd>Bachelor of Computer Science (Software Development)</dd>
+            
+            
+                <dt>STUDENT EMAIL:</dt>
+                <dd>
+                    <a href="mailto:104584773@student.swin.edu.au">
+                        104584773@student.swin.edu.au
+                    </a>
+                </dd>
+
+                <dt>SPORTS:</dt>
+                <dd>ice skating, swimming</dd>
+
+                <dt>HOBBIES:</dt>
+                <dd>reading, watching movies</dd>
+            </dl> 
+            <img src="images/lorraine.jpeg" alt="lorraine">
+
+        </div>
+        <div class="row2">
+            <table>
+                <tr>
+                        <th>monday</th>
+                        <th>tuesday</th>
+                        <th>wednesday</th>
+                        <th>thursday</th>
+                        <th>friday</th>
+                </tr>
+                <tr>
+                    <td>COS20031 Live online <br> 10:30-11:30
+                    </td>
+
+                    <td>COS10009 Lecture 1 <br> 10:30-12:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10026 Workshop 1 <br> 10:30-12:30
+                    </td>
+
+                    <td>TNE10006 Lecture 1 <br> 10:30-12:30
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>COS10026 Live online <br>  12:30-1:30
+                    </td>
+
+                    <td>COS10026 Class 1 <br> 3:30-4:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10009 Class 1 <br> 12:30-2:30
+                    </td>
+                    
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>COS20031 Class 1 <br> 4:30-5:30
+                    </td>
+
+                    <td></td>
+
+                    <td>TNE10006 Class 1 <br> 2:30-5:30
+                    </td>
+
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                    <td>COS20031 Workshop 1 <br> 6:30-8:30
+                    </td>
+
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+        
+
+    </div>
+     
+
+    <div class="profile">
+        <div class="row1">
+            <dl>
+                <dt>STUDENT NAME:</dt>
+                <dd>Muy Houng Leang</dd>
+        
+                <dt>STUDENT ID:</dt>
+                <dd>104356422</dd>
+                
+        
+                <dt>COURSE:</dt>
+                <dd>Bachelor of Computer Science (Software Development)</dd>
+                
+        
+                <dt>STUDENT EMAIL:</dt>
+                <dd>
+                    <a href="mailto:104356422@student.swin.edu.au">
+                        104356422@student.swin.edu.au
+                    </a>
+                </dd>
+
+                <dt>SPORTS:</dt>
+                <dd>none</dd>
+                
+                <dt>HOBBIES:</dt>
+                <dd>baking, reading, watching tv shows</dd>
+            </dl>
+
+            <img src="images/muyhoung.jpeg" alt="muyhoung">
+        </div>
+        <div class="row2">
+            <table>
+                <tr>
+                        <th>monday</th>
+                        <th>tuesday</th>
+                        <th>wednesday</th>
+                        <th>thursday</th>
+                        <th>friday</th>
+                </tr>
+                <tr>
+                    <td>COS20031 Live online <br> 10:30-11:30
+                    </td>
+
+                    <td>COS10009 Lecture 1 <br> 10:30-12:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10026 Workshop 1 <br> 10:30-12:30
+                    </td>
+
+                    <td>TNE10006 Lecture 1 <br> 10:30-12:30
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>COS10026 Live online <br>  12:30-1:30
+                    </td>
+
+                    <td>COS10026 Class 1 <br> 3:30-4:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10009 Class 1 <br> 12:30-2:30
+                    </td>
+                    
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>COS20031 Class 1 <br> 4:30-5:30
+                    </td>
+
+                    <td></td>
+
+                    <td>TNE10006 Class 1 <br> 2:30-5:30
+                    </td>
+
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                    <td>COS20031 Workshop 1 <br> 6:30-8:30
+                    </td>
+
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+    </div>
+
+    <div class="profile">
+        <div class="row1">
+            <dl>
+                <dt>STUDENT NAME:</dt>
+                <dd>Mahdi Rezaie</dd>
+            
+            
+                <dt>STUDENT ID:</dt>
+                <dd>103980338</dd>
+                
+            
+                <dt>COURSE:</dt>
+                <dd>Bachelor of Computer Science (Software Development)</dd>
+            
+            
+                <dt>STUDENT EMAIL:</dt>
+                <dd>
+                    <a href="mailto:103980338@student.swin.edu.au">
+                        103980338@student.swin.edu.au
+                    </a>
+                </dd>
+
+                <dt>SPORTS:</dt>
+                <dd>swimming, walking</dd>
+
+                <dt>HOBBIES:</dt>
+                <dd>reading</dd>
+            </dl> 
+            <img src="images/mahdi.jpeg" alt="mahdi">
+
+        </div>
+        <div class="row2">
+            <table>
+                <tr>
+                        <th>monday</th>
+                        <th>tuesday</th>
+                        <th>wednesday</th>
+                        <th>thursday</th>
+                        <th>friday</th>
+                </tr>
+                <tr>
+                    <td>COS20031 Live online <br> 10:30-11:30
+                    </td>
+
+                    <td>COS10009 Lecture 1 <br> 10:30-12:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10026 Workshop 1 <br> 10:30-12:30
+                    </td>
+
+                    <td>TNE10006 Lecture 1 <br> 10:30-12:30
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>COS10026 Live online <br>  12:30-1:30
+                    </td>
+
+                    <td>COS10026 Class 1 <br> 3:30-4:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10009 Class 1 <br> 12:30-2:30
+                    </td>
+                    
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>COS20031 Class 1 <br> 4:30-5:30
+                    </td>
+
+                    <td></td>
+
+                    <td>TNE10006 Class 1 <br> 2:30-5:30
+                    </td>
+
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                    <td>COS20031 Workshop 1 <br> 6:30-8:30
+                    </td>
+
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+        
+
+    </div>
+        
+    <div class="profile">
+        <div class="row1">
+            <dl>
+                <dt>STUDENT NAME:</dt>
+                <dd>Corey Davis</dd>
+                <dt>STUDENT ID:</dt>
+                <dd>104567570</dd>
+                
+            
+                <dt>COURSE:</dt>
+                <dd>Bachelor of Computer Science (Software Development)</dd>
+                
+            
+                <dt>STUDENT EMAIL:</dt>
+                <dd>
+                    <a href="mailto:104567570@student.swin.edu.au">
+                        104567570@student.swin.edu.au
+                    </a>
+                </dd>
+
+                <dt>SPORTS:</dt>
+                <dd>none</dd>
+
+                <dt>HOBBIES</dt>
+                <dd>drawing, reading, learning languages</dd>
+            </dl>
+
+            <img src="images/corey.jpeg" alt="corey">
+
+        </div>
+        <div class="row2">
+            <table>
+                <tr>
+                        <th>monday</th>
+                        <th>tuesday</th>
+                        <th>wednesday</th>
+                        <th>thursday</th>
+                        <th>friday</th>
+                </tr>
+                <tr>
+                    <td>COS20031 Live online <br> 10:30-11:30
+                    </td>
+
+                    <td>COS10009 Lecture 1 <br> 10:30-12:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10026 Workshop 1 <br> 10:30-12:30
+                    </td>
+
+                    <td>TNE10006 Lecture 1 <br> 10:30-12:30
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>COS10026 Live online <br>  12:30-1:30
+                    </td>
+
+                    <td>COS10026 Class 1 <br> 3:30-4:30
+                    </td>
+
+                    <td></td>
+
+                    <td>COS10009 Class 1 <br> 12:30-2:30
+                    </td>
+                    
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td>COS20031 Class 1 <br> 4:30-5:30
+                    </td>
+
+                    <td></td>
+
+                    <td>TNE10006 Class 1 <br> 2:30-5:30
+                    </td>
+
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+
+                    <td>COS20031 Workshop 1 <br> 6:30-8:30
+                    </td>
+
+                    <td></td>
+                </tr>
+            </table>
+
+        </div>
+        
+        
+    </div>
+
+       
+<?php
+include 'footer.inc';
+?>
+
+</body>
+
+</html>
+
---
+++ assign2/assign2/jobs.php
@@ -0,0 +1,136 @@
+
+<?php 
+$title="Job Descriptions";
+include 'header.inc';
+?> 
+
+<body>
+
+<?php 
+include 'menu.inc';
+?>
+    <hr />
+
+     <div class="job-container" id="job-container-1">
+         <div class="jobtitle">
+             <h1>Junior Web Developer Position (08923)</h1>
+
+         </div>
+            <div class="JobMisc">
+                <h5>Full time employment (40 Hours per week) </h5>
+                <h5>Average Yearly Salary (~$85,000)</h5>
+            </div>
+            <div class=paragraphbody>
+                    <aside>
+                        <p>Web Developers at Code Crafters are expected to report directly to Senior Web Developer "Sarah Williams", at their earliest convenience if major issues in relation to to other staff members or ongoing projects occur. </p>
+                    </aside>
+                <p> Here at Code Crafters, we are seeking an experienced and results driven Web Developer to help assist with both ongoing and future projects.
+                A typical day at Code Crafters would be spent working with other team members to create consumer friendly websites that are simple yet effective. 
+                Whilst also maintaining and troubleshooting various bugs and issues that may arise throughout development.
+                Web Developer positions are flexible, accommodating both local and remote workers. Through the use of various sites such as Github and Zoom, workers are easily able to discuss and share ideas regardless of their location or situation.
+                Despite this it is expected that projects are still completed both on-time and to company standards. Code Crafters is renowned for their team's strong work ethic and meticulous attention to detail, as such it is important for all work to be completed on time and to a exceptional level to ensure 
+                customer satisfaction.</p>
+         </div>
+            <section class="responsibilities">
+                    <h2>Responsibilities</h2>
+                        <ol>
+                            <li>Completing work on-time and to Company standards</li>
+                            <li>Producing well written and structured code</li>
+                            <li>Determining and incorporating consumer needs</li>
+                            <li>Troubleshooting and Bugfixing issues </li>
+                            <li>Maintaining strong work ethic</li>
+                        </ol>
+            </section>
+            <section class="jobslist">
+                    <h2>Essential</h2>
+                    <ul>
+                        <li>In-depth knowledge of HTML and CSS</li>
+                        <li>Proficiency in programming languages such as: Java, Ruby, Python and C#</li>
+                        <li>Strong Understanding of Ui and Ux</li>
+                        <li>Testing and Debugging skills</li>
+                        <li>A Degree in Computer Science or similar related field</li>
+                    </ul>
+            </section>
+            <section class="jobslist">
+                    <h2>Desirable</h2>
+                    <ul>
+                        <li>Previous work experience (Provided via Portfolio)</li>
+                        <li>Proficiency with Adobe Suite</li>
+                        <li>Good communication skills</li>
+                        <li>Ability to multitask effectively</li>
+                        <li>Keen attention to detail</li>
+                    </ul>
+                    <div class="apply-button"><span> <a href="apply.php">Apply Now!</a> </span></div > 
+            </section>
+
+        </div>
+    
+        <hr>
+        
+        <div class="job-container">
+            <div class="jobtitle">
+                <h1> Senior Network Engineer (06782)</h1> 
+            </div> 
+            
+            <div class="JobMisc">
+               <h5>Full Time Employment (42 Hours per week)</h5> 
+                <h5>Average Yearly Salary (~$110,000)</h5>
+            </div>
+    
+            <div class="paragraphbody">
+                
+                <aside>
+                    <p>Network Engineers are expected to report to the Director of Network Management "Patrick Smith", at their earliest convenience if major issues relating to company networks or hardware occur.</p>
+                </aside>
+                    
+                <p>Code Crafters is looking for an experienced and efficient industry veteran who is able to work effectively both individually and in a team. 
+                    Our Network Engineers are able to efficiently develop and optimize company networks and systems to ensure easy access for all current and future team members. 
+                    A typical work day will consist of individual and collaborative troubleshooting along with providing support regarding company networks to other team members, whilst ensuring all systems are working correctly.  
+                    Due to the nature of Network Engineering, all workers are required to be on-site should issues to company hardware arise. Code Crafters is renowned for their team's strong work ethic and meticulous attention to detail, as such it is important for all work to be completed on time and to a exceptional level to ensure 
+                    customer satisfaction. </p>
+            </div>
+          
+
+            <section class="responsibilities">
+                    <h2> Responsibilities </h2>
+                    <ol>
+                        <li>Creating Computer Networks that will be utilized by several company branches</li>
+                        <li>Troubleshooting issues relating to both hardware and networks</li>
+                        <li>Managing Company LANS, WLANS and WANS</li>
+                        <li>Reporting severe issues regarding either hardware or software to upper management</li>
+                    </ol>
+                   
+            </section>
+            <section class="jobslist">
+                    <h2> Essential </h2>
+                    <ul>
+                        <li>Bachelor's (or higher) degree in either Computer Science or Information Technology</li>
+                        <li>Cisco CCNA Cert or equivalent</li>
+                        <li>Proficiency in both high level Mathematics and Physics</li>
+                        <li>Understanding of both code and Networking structures</li>
+                        <li>Previous Networking experience</li>
+                    </ul>
+                   
+            </section>
+            <section  class="jobslist">
+                    <h2> Desirable </h2>
+                    <ul>
+                        <li>Strong time management skills</li>
+                        <li>Able to work effectively both individually and in a group</li>
+                        <li>Ability to document and record previous and ongoing network issues</li>
+                        <li>Good communication skills</li>
+                        <li>Problem solving skills</li>
+                    </ul>
+
+                    <div class="apply-button"><span> <a href="apply.php">Apply Now!</a> </span></div > 
+            </section>
+            
+            
+        </div>
+
+<?php
+include 'footer.inc';
+?>
+
+</body>
+</html>
