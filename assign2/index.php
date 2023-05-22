c176c82cd9976adbdf78295c973029a9cd9fc76a
wafflesmilk <126733437+wafflesmilk@users.noreply.github.com>
2023-05-22 11:28:40 +1000

    Add files via upload

---
+++ assign2/index.php
@@ -0,0 +1,28 @@
+
+<?php 
+$title="Welcome to Code Crafters";
+include 'header.inc';
+?> 
+
+<body>
+
+
+<?php 
+include 'menu.inc';
+?>
+
+    <div class="text">
+        <div class="maintext">
+            <h1> Code Crafters. </h1>
+            <h3> As Australia's leading software development company, Code Crafters are currently seeking new team members to join our organisation. </h3>
+            <div class="job-button"><a href="jobs.html">Search Jobs</a></div>
+        </div>
+        <div class="expand"></div>
+    </div>
+
+<?php
+include 'footer.inc';
+?>
+
+</body>
+</html>
